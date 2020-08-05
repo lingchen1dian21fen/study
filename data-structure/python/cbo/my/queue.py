7ebccbaa923506a3d758f7b9768ba4fdcdb33d40
chengbo <chengbo@chengbo>
2020-08-05 20:12:05 +0800

    1.实现循环队列

--- data-structure/python/cbo/my/queue.py
+++ data-structure/python/cbo/my/queue.py
@@ -66,6 +66,49 @@ class LinkedQueue(object):
         if self.__len__() == 0:
             return None
         return self._data.first()
+
+class CircularQueue(object):
+    
+    def __init__(self, maxsize):
+        self._data = [None] * (maxsize + 1)
+        self._maxsize = (maxsize + 1)
+        self._font = 0
+        self._rear = 0
+    
+    def __len__(self):
+        return (self._rear - self._font + self._maxsize) % self._maxsize
+    
+    def __iter__(self):
+        i = 0
+        while i < self.__len__():
+            yield self._data[(self._font + i) % self._maxsize]
+            i += 1
+        return None 
+    
+    def is_empty(self):
+        return self.__len__() == 0
+    
+    def is_full(self):
+        return self.__len__() == self._maxsize - 1
+    
+    def push(self, value):
+        if self.is_full():
+            raise IndexError('queue is full!')
+        self._data[self._rear] = value
+        self._rear = (self._rear + 1) % self._maxsize
+    
+    def pop(self):
+        if self.is_empty():
+            raise IndexError('queue is empty')
+        val = self._data[self._font]
+        self._data[self._font] = None
+        self._font = (self._font + 1) % self._maxsize
+        return val
+    
+    def peek(self):
+        if self.is_empty():
+            raise IndexError('queue is empty')
+        return self._data[self._font]
     
     
 def assert_sequence():
@@ -97,9 +140,34 @@ def assert_lined():
     for data in queue:
         print(data)
     print('链表队列 断言结束')
+    
+def assert_circular():
+    queue = CircularQueue(5)
+    assert queue.is_empty() == True
+    queue.push('aaa')
+    queue.push('bbb')
+    queue.push('ccc')
+    assert len(queue) == 3
+    queue.push('ddd')
+    queue.push('eee')
+    assert queue.is_full() == True
+    assert queue.pop() == 'aaa'
+    assert queue.pop() == 'bbb'
+    assert queue.peek() == 'ccc'
+    assert len(queue) == 3
+    queue.push('fff')
+    queue.push('ggg')
+    assert len(queue) == 5
+    for data in queue:
+        print(data)
+    print('循环队列 断言结束')
+    
+    
 
 if __name__ == '__main__':
     # 顺序队列
     assert_sequence()
     # 链表队列 
     assert_lined()
+    # 循环队列 
+    assert_circular()
