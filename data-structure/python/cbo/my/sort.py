dbd2d448899dc6a5e0bea1cec1e4869734751480
chengbo <chengbo@chengbo>
2020-08-11 10:13:48 +0800

    1添加插入排序实现 2添加归并排序实现

--- data-structure/python/cbo/my/sort.py
+++ data-structure/python/cbo/my/sort.py
@@ -13,6 +13,7 @@ Created on 2020-08-07 15:08
             问题：编程实现O(n)时间复杂度内找到一组数据的第K大元素    
 
 '''
+from math import floor
 
 def bubble_sort(datas):
     for i in range(len(datas) - 1):
@@ -32,10 +33,39 @@ def selection_sort(datas):
     return datas        
 
 def merge_sort(datas):
-    pass
+    if len(datas) < 2:
+        return datas
+    middle = floor(len(datas) / 2)
+    left = datas[:middle:]
+    rigth = datas[middle::]
+    return _merge(merge_sort(left), merge_sort(rigth))
+
+def _merge(left, right):
+    res = []
+    while left and right:
+        if left[0] <= right[0]:
+            res.append(left.pop(0))
+        else:
+            res.append(right.pop(0))
+    while left:
+        res.append(left.pop(0)) 
+    while right:
+        res.append(right.pop(0))
+    return res
 
 def insert_sort(datas):
-    pass
+    if len(datas) < 2:
+        return 
+    i = 1
+    while i < len(datas):
+        j = i
+        while j > 0:
+            if datas[j] >= datas[j - 1]:
+                break
+            else:
+                datas[j], datas[j - 1] = datas[j - 1], datas[j]
+                j -= 1            
+        i += 1
 
 def quick_sort(datas):
     pass
@@ -58,8 +88,28 @@ def assert_selection():
     assert datas[8] == 9
     print(datas)
     print('选择排序  测试结束')
+    
+def assert_merge():
+    datas = [3, 5, 1, 8, 2, 4, 9, 6, 7]
+    res = merge_sort(datas)
+    assert res[0] == 1
+    assert res[4] == 5
+    assert res[8] == 9
+    print(res)
+    print('归并排序  测试结束')    
+
+def assert_insert():
+    datas = [3, 5, 1, 8, 2, 4, 9, 6, 7]
+    insert_sort(datas)
+    assert datas[0] == 1
+    assert datas[4] == 5
+    assert datas[8] == 9
+    print(datas)
+    print('插入排序  测试结束')
 
 if __name__ == '__main__':
     assert_bubble()
     assert_selection()
+    assert_merge()
+    assert_insert()
     
